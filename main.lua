575a263a9f4852b4b95e468d511b456f0a8ccbd7
Cicero-byte <12222016t@gmail.com>
2025-05-26 01:16:52 -0300

    Mostra um quadrado que pode se mover

--- main.lua
+++ main.lua
@@ -1,4 +1,33 @@
-function love.draw()
-    love.graphics.print("Hello World", 400, 300)
+function love.load()
+    x, y, h, w = 100, 200, 50,50
+
 end
 
+function love.update()
+    right = love.keyboard.isDown("d")
+    left = love.keyboard.isDown("a")
+    up = love.keyboard.isDown("w")
+    down = love.keyboard.isDown("s")
+
+    if right then
+        x = x + 20
+    end
+
+    if left then
+        x = x - 20
+    end
+    if up then
+        y = y - 20
+    end
+    if down then
+        y = y + 20
+    end
+
+
+
+end
+
+function love.draw()
+    love.graphics.rectangle("line", x, y, h, w)
+
+end
